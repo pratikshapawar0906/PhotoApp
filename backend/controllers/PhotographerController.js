b18fb0d34ee6a0b3bface40081deb7bc27744391
Pratiksha Pawar <pratikshapawar920@gmail.com>
2025-01-24 19:54:31 +0530

    login

--- backend/controllers/PhotographerController.js
+++ backend/controllers/PhotographerController.js
@@ -40,7 +40,7 @@ exports.login1=async (req, res) => {
         return res.status(400).json({ message: "Invalid email or password" });
       }
   
-      const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1h" });
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
       res.status(200).json({ message: "Login successful", token });
     } catch (err) {
       res.status(500).json({ message: "Server error" });
